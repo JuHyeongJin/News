b469f04d83adc95a4da43c1b9f397a9631428a7b
JuHyeongJin <wngudwlswkd@gmail.com>
2022-06-01 16:07:39 +0900

    Make html and css about title and menu

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 // newscatcher api key: nZlMohtufaB7zpiLzjKbF8E_IfynocouWHUnQ-_2y8w
+
+let news = [];
+
+const getLatestNews = async()=>{
+    let url = new URL('https://api.newscatcherapi.com/v2/latest_headlines?countries=US&topic=business&page_size=2');
+    let header = new Headers({'x-api-key':'nZlMohtufaB7zpiLzjKbF8E_IfynocouWHUnQ-_2y8w'});
+
+    let response = await fetch(url,{headers:header}); //ajax, http, fetch 등 사용가능
+    let data = await response.json();
+
+    news = data.articles;
+    console.log(news);
+
+}
+
+getLatestNews();
