01d36757ce2651a587609533a274938a8c0d4a2b
JuHyeongJin <wngudwlswkd@gmail.com>
2022-06-09 18:40:41 +0900

    Make slide menu-bar

--- main.js
+++ main.js
@@ -148,5 +148,13 @@ const openSearchBox = () => {
   }
 };
 
+const openNav = () => {
+  document.getElementById("mySidenav").style.width = "250px";
+};
+
+const closeNav = () => {
+  document.getElementById("mySidenav").style.width = "0";
+};
+
 searchButton.addEventListener("click", getNewsByKeyword);
 getLatestNews();
